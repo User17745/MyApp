9d87834bf173691dde66aeb76e2d5fed2571d3ab
User17745 <AbhishekAggarwal1995@gmail.com>
2016-01-26 00:27:57 +0530

    1. Did Shipping Calculator module but incomplete and also encounters an error and crashes the app. 2. Did the error page for webView. 3. Minor fixes.

--- app/src/main/java/com/technotronics/priceconverter/PDF.java
+++ app/src/main/java/com/technotronics/priceconverter/PDF.java
@@ -5,20 +5,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class PDF extends AppCompatActivity {
 
+
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    WebView mWebView=new WebView(PDF.this);
-	    mWebView.getSettings().setJavaScriptEnabled(true);
+
+		  WebView mWebView=new WebView(PDF.this);
+
+		  mWebView.getSettings().setJavaScriptEnabled(true);
+
 		  mWebView.setWebViewClient(new WebViewClient());
-	    //mWebView.getSettings().setPluginsEnabled(true);
-	    //mWebView.loadUrl("https://www.luminpdf.com/documents/oZT3fQ66ocrjSBEcz/share?sk=b4fd3eee-6e87-425a-87f3-d0522a06d225");
+		  //mWebView.getSettings().setPluginsEnabled(true);
+	      //mWebView.loadUrl("https://www.luminpdf.com/documents/oZT3fQ66ocrjSBEcz/share?sk=b4fd3eee-6e87-425a-87f3-d0522a06d225");
 		  mWebView.loadUrl("https://docs.zoho.com/file/v7xm916ea0e7bd5544edfa210afd98f01fc4a");
 		  setContentView(mWebView);
 
+		  mWebView.setWebViewClient(new WebViewClient() {
+			  @Override
+			  public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+			  {
+				  setContentView(R.layout.errorweb);
+			  }
+		  });
 	  }
 	}
--- app/src/main/java/com/technotronics/priceconverter/ShippingCalc.java
+++ app/src/main/java/com/technotronics/priceconverter/ShippingCalc.java
@@ -2,38 +2,54 @@ package com.technotronics.priceconverter;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShippingCalc extends AppCompatActivity{
 
+	Float ShipCost;
+	int BaseRate, AddRate;
+	RadioButton Delhi = (RadioButton) findViewById(R.id.city);
+	RadioButton Zone = (RadioButton) findViewById(R.id.zone);
+	RadioButton Metro = (RadioButton) findViewById(R.id.metro);
+	RadioButton ROI = (RadioButton) findViewById(R.id.roi);
+	CheckBox Rural = (CheckBox) findViewById(R.id.ruralCheck);
+
 	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shipping);
-		
+
 		Button ShipRes = (Button) findViewById(R.id.shipCalc);
 		ShipRes.setOnClickListener(Calculater);
-		
+
 	}
-	
+
 	private OnClickListener Calculater = new OnClickListener()
 	{
 
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
+
+			//----KEYBOARD DOWN-------------//
 			InputMethodManager inputManager = (InputMethodManager)
             getSystemService(Context.INPUT_METHOD_SERVICE); 
-			inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
-			
+			//inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+			//----KEYBOARD DOWN-------------//
+
+			//----PARTS CALCULATION-------------//
 			EditText Weight = (EditText) findViewById(R.id.Weight);
 			 String Wei = Weight.getText().toString();
 			   if(Wei.matches(""))
@@ -49,12 +65,50 @@ public class ShippingCalc extends AppCompatActivity{
 			 {
 				 Parts++;
 			 }
-			 
+			//----PARTS CALCULATION-------------//
+
+			//----REGION SELECTION-------------//
+			RadioGroup Region = (RadioGroup) findViewById(R.id.regionSelect);
+			Region.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+				@Override
+				public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+					if (checkedId == R.id.city) {
+						BaseRate = 32;
+						AddRate = 27;
+					} else if (checkedId == R.id.zone) {
+						BaseRate = 37;
+						AddRate = 32;
+					} else if (checkedId == R.id.metro) {
+						BaseRate = 47;
+						AddRate = 42;
+					} else {
+						BaseRate = 52;
+						AddRate = 47;
+					}
+
+				}
+			});
+			//----REGION SELECTION-------------//
+
+			Float Sum = (float) BaseRate;
+			for(int i = 0 ; i < Parts ; i++)
+			{
+				Sum += AddRate;
+			}
+			Sum += 10; //Fuel Charges
+
+			ShipCost = Sum;
+
 			TextView Result = (TextView) findViewById(R.id.shipping);
-			 Result.setText(Parts.toString());
-			 
-			 
+
+			if(Rural.isChecked())
+				ShipCost += 50;
+
+			Result.setText(ShipCost.toString());
+
 		}
-		
+
+
 	};
 }
